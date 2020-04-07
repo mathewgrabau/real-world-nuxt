f31ea8a26a8052ef67bf0dce51367b27814d2701
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-04-06 23:32:01 -0500

    Implemented the asyncData pulling data from server and components needed.

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
     <h1>Events</h1>
+
+    <EventCard
+      v-for="(event, index) in events"
+      :key="index"
+      :event="event"
+      :data-index="index"
+    />
   </div>
 </template>
 
 <script>
+import EventCard from '@/components/EventCard.vue'
+
 export default {
+  components: { EventCard },
+  // Using ES6 destructuring to unpack the context
+  asyncData({ $axios }) {
+    return $axios.get('http://localhost:3000/events').then((response) => {
+      return {
+        events: response.data
+      }
+    })
+  },
   head() {
     return {
       title: 'Event Listing'
