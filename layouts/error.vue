17e65a9c28ae69836df8848089de0bc79c741cb5
Mathew Grabau <mgrabau@mathewgrabau.ca>
2020-04-06 21:15:29 -0500

    Implemented an error layout.

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="title">{{ message }}</div>
+    <p v-if="statusCode === 404">
+      <nuxt-link to="/">Return to homepage</nuxt-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message() {
+      return this.error.message
+    }
+  },
+  head() {
+    return {
+      title: this.message
+    }
+  }
+}
+</script>
